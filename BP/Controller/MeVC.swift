c76ef4e8e4d667dadf7e90814faf40f91a20fc84
Jesse Elkharrat <jessenelk@gmail.com>
2018-05-31 19:51:04 -0400

    S10L145

---
+++ BP/Controller/MeVC.swift
@@ -0,0 +1,21 @@
+//
+//  MeVC.swift
+//  BP
+//
+//  Created by Nessin Elkharrat on 5/31/18.
+//  Copyright © 2018 practice. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBOutlet weak var signOutButtonWasPressed: UIButton!
+}
